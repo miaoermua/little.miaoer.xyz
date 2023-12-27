a085f13477d3cb370c0041a9c8447563dbecc639
喵二 <66155308+miaoermua@users.noreply.github.com>
2023-12-27 15:09:10 +0800

    Update cattools.sh

--- cattools/cattools.sh
+++ cattools/cattools.sh
@@ -24,11 +24,11 @@ fi
 release=$(cat /etc/openwrt_release)
 
 if [[ $release =~ "OpenWrt" ]]; then
-  echo "$(date) - Starting CatWrt Network Diagnostics"  
+    echo "$(date) - Starting CatWrt Network Diagnostics"  
 else
-  echo "Abnormal system environment..."
-  echo " "
-  exit 1
+    echo "Abnormal system environment..."
+    echo " "
+    exit 1
 fi
 
 setip(){
@@ -126,7 +126,7 @@ debug(){
         fi
 
            cat /etc/banner > /www/logs.txt
-           cat /etc/catwrt_release > /www/logs.txt
+           cat /etc/catwrt_release >> /www/logs.txt
            logread >> /www/logs.txt
            dmesg >> /www/logs.txt
 
@@ -142,11 +142,11 @@ bypass_gateway(){
         echo "没做完"
         exit
 }
-	
-update
+
 
 while :; do
     clear
+    echo " "
     echo "                CatTools"
     echo "----------------------------------------"  
     echo "1.  Set IPv4 Addr          设置 IP"
